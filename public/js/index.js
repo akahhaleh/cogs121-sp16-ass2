17489aaba7093e51ff6dfd780ba91f5d3d11b3fa
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-04-24 13:17:37 -0700

    part 1 d3 done

--- public/js/index.js
+++ public/js/index.js
@@ -24,8 +24,8 @@
   var innerHeight = height - margin.top  - margin.bottom;
 
   // TODO: Input the proper values for the scales
-  var xScale = d3.scale.ordinal().rangeRoundBands([0, 10], 0);
-  var yScale = d3.scale.linear().range([30, 0]);
+  var xScale = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+  var yScale = d3.scale.linear().range([height, 0]);
 
   // Define the chart
   var chart = d3
@@ -40,21 +40,25 @@
   xScale.domain(data.map(function (d){ return d.name; }));
 
   // TODO: Fix the yScale domain to scale with any ratings range
-  yScale.domain([0, 5]);
+  yScale.domain([0, d3.max(data, function(d) {
+        return d.rating;
+    })]);
 
+
+  console.log("the data is: " + data[1].rating);
   // Note all these values are hard coded numbers
   // TODO:
   // 1. Consume the taco data
   // 2. Update the x, y, width, and height attributes to appropriate reflect this
   chart
     .selectAll(".bar")
-    .data([10, 20, 30, 40])
+    .data(data)
     .enter().append("rect")
     .attr("class", "bar")
-    .attr("x", function(d, i) { return i*100; })
-    .attr("width", 100)
-    .attr("y", function(d) { return 0; })
-    .attr("height", function(d) { return d*10; });
+    .attr("x", function(d, i) { return xScale(d.name) })
+    .attr("width", xScale.rangeBand())
+    .attr("y", function(d) { return yScale(d.rating) })
+    .attr("height", function(d) {return height -yScale(d.rating)});
 
   // Orient the x and y axis
   var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
@@ -62,12 +66,19 @@
 
   // TODO: Append X axis
   chart
-    .append("g");
-
+        .append("g").attr("class", "x axis")
+        .call(xAxis)
+        .attr("transform", "translate(0," + height + ")")
+        .selectAll("text")  
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-65)" );
 
   // TODO: Append Y axis
-  chart
-    .append("g");
+  chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
 
 
   // ASSIGNMENT PART 1B
